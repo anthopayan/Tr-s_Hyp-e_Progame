e15bc8865bab1ac71bcab6da1f57036555e1788f
anthopayan <76994877+anthopayan@users.noreply.github.com>
2021-04-29 21:29:10 +0200

    Update PageList.js

--- src/js/PageList.js
+++ src/js/PageList.js
@@ -1,80 +1,95 @@
-var searchbtn = document.getElementById('searchbtn');
-var searchuser = "";
-
-document.getElementById('searchbtn').addEventListener('click', () => {
-  console.log("click")
-  var searchuser = document.getElementById('searchuser');
-  argument = searchuser.value; 
-  PageList(argument);
-});
+const PageDetail = (argument) => {
+  const preparePage = () => {
+    let cleanedArgument = argument.replace(/\s+/g, "-");
 
 
-const PageList = (argument) => {
-  
-    const preparePage = () => {
-      let cleanedArgument = argument.replace(/\s+/g, "-");
-      let articles = "";
-      let btn = "";
-   
+    const fetchGame = (url, argument) => {
+      let finalURL = url + argument + "?key=" + process.env.KEY;
+console.log(argument);
+      fetch(`${finalURL}`)
+        .then((response) => response.json())
+        .then((response) => {
+          let { genres, stores, rating, ratings_count, publishers, tags, platforms, developers, name, released, description, website } = response;
 
-     
-      
-      const fetchList = (url, argument, count) => {
-        let finalURL = url;
-        if (argument) {
+          let articleDOM = document.querySelector(".page-detail .article");
+          articleDOM.querySelector("h1.title").innerHTML = name;
+          articleDOM.querySelector("p.release-date span").innerHTML = released;
+          articleDOM.querySelector("p.description").innerHTML = description;
+          articleDOM.querySelector("a.site").innerHTML = website;
+          articleDOM.querySelector("p.rating").innerHTML = rating;
+          articleDOM.querySelector("p.ratings_count").innerHTML = ratings_count;
           
-          finalURL = url + "&search=" + argument;
-        }
-       
-
-
-        
-        fetch(`${finalURL}`)
-          .then((response) => response.json())
-          .then((response) => {
-            response.results.forEach((article) => {
-              articles += `
-                    <div class="cardGame">
-                      <h1>${article.name}</h1>
-                      <h2>${article.released}</h2>
-                      <a href = "#pagedetail/${article.id}">${article.id}</a>
-                    </div>
-                  `;
-            });
-            if (count < 0);{
-            btn += `
-            <div>
-            <button id="showmore">Show more</button>
-            </div>
-          `
-       
-        }
- 
-
-            document.querySelector(".page-list .articles").innerHTML = articles;
-            document.getElementById(`showmore`).innerHTML = btn;
+          articleDOM.querySelector("a.site").href = website;
+          var listpublisher = [];
+          publishers.forEach((publisher) => {
+            listpublisher.push(publisher.name);
           });
-      };
- 
-      fetchList("https://api.rawg.io/api/games?key=" + process.env.KEY, cleanedArgument);
+          articleDOM.querySelector("p.publishers").innerHTML = listpublisher.join(', ');
+          var listtag = [];
+          tags.forEach((tag) => {
+            listtag.push(tag.name);
+          });
+          articleDOM.querySelector("p.tags").innerHTML = listtag.join(', ');
+          var listgenre = [];
+          genres.forEach((genre) => {
+            listgenre.push(genre.name);
+          });
+          articleDOM.querySelector("p.genres").innerHTML = listgenre.join(', ');
+          var listdeveloper = [];
+          developers.forEach((developer) => {
+            listdeveloper.push(developer.name);
+          });
+          articleDOM.querySelector("p.developers").innerHTML = listdeveloper.join(', ');
+          var listplatform = [];
+          platforms.forEach((platform) => {
+            listplatform.push(platform.platform.name);
+          });
+          articleDOM.querySelector("p.platforms").innerHTML = listplatform.join(', ');
+          var liststore = [];
+          stores.forEach((store) => {
+            liststore.push(`http://www.${store.store.domain}`);
+          });
+          articleDOM.querySelector("a.stores").innerHTML = liststore.join(', ');
+        });
     };
-    
 
-  
-    const render = () => {
-      pageContent.innerHTML = `
-        <section class="page-list">
-          <div class="articles">...loading</div>
-        </section>
-      `
-  
-
-    
-      preparePage();
-    };
-  
-    render();
+    fetchGame("https://api.rawg.io/api/games/", cleanedArgument);
   };
-  export { PageList };
 
-  
+  const render = () => {
+    pageContent.innerHTML = `
+      <section class="page-detail">
+        <div class="article">
+          <h1 class="title"></h1>
+          <p class="description"></p>
+          <p>Release Date:</p>
+          <p class="release-date">Release date : <span></span></p>
+          <p>Publisher:</p>
+          <p class="publishers"></p>
+          <p>Tags:</p>
+          <p class="tags"></p>
+          <p>Genre:</p>
+          <p class="genres"></p>
+          <p>Developer:</p>
+          <p class="developers"></p>
+          <p>Platforms:</p>
+          <p class="platforms"></p>
+          <p>Moyenne:</p>
+          <p class="rating"></p>
+          <p>Votes:</p>
+          <p class="ratings_count"></p>
+          <p>Site:</p>
+          <a class="site"></a>
+          <p>BUY:</p>
+          <a class="stores" ></a>
+        </div>
+      </section>
+    `;
+
+    preparePage();
+  };
+
+  render();
+};
+
+export { PageDetail };
